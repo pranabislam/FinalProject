759cff7965799e819f1e986dc452a4f7b455a51a
Pranab <Islam>
2015-01-12 20:07:09 -0500

    still dont know what's wrong with the for loop

--- Gui1.java
+++ Gui1.java
@@ -7,90 +7,90 @@ import java.io.File;
 
 public class Gui1 extends JFrame {
 
-		private JButton b1,b2;
-		private Container pane;
-		private JLabel label;
-		private JTextArea text;
-		private JPanel canvas;
+    private JButton b1,b2;
+    private Container pane;
+    private JLabel label;
+    private JTextArea text;
+    private JPanel canvas;
 		
-		public Gui1(){
+    public Gui1(){
 				
-				setTitle("My First Gui");
-				setSize(600,600);
-				setLocation(100,100);
-				setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setTitle("My First Gui");
+	setSize(600,600);
+	setLocation(100,100);
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-				pane = getContentPane();
-				//pane.setLayout(new GridLayout(3,3));
-				pane.setLayout(new FlowLayout());
-				b1 = new JButton("Click me");
-				pane.add(b1);
-				b2 = new JButton("exit");  
-				pane.add(b2);
+	pane = getContentPane();
+	//pane.setLayout(new GridLayout(3,3));
+	pane.setLayout(new FlowLayout());
+	b1 = new JButton("Click me");
+	pane.add(b1);
+	b2 = new JButton("exit");  
+	pane.add(b2);
 
-				label = new JLabel("The Label:");
-				pane.add(label);
+	label = new JLabel("The Label:");
+	pane.add(label);
 
-				text = new JTextArea();
-				text.setColumns(40);
-				text.setRows(5);
-				text.setBorder(BorderFactory.createLineBorder(Color.red,2));
-				pane.add(text);
+	text = new JTextArea();
+	text.setColumns(40);
+	text.setRows(5);
+	text.setBorder(BorderFactory.createLineBorder(Color.red,2));
+	pane.add(text);
 
-				canvas = new JPanel();
-				canvas.setPreferredSize(new Dimension(300,300));
-				canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
-				canvas.setLocation(10,10);
-				BufferedImage myPicture = null;
-				try {
-				    myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
-				} catch(Exception e)
-				    { System.out.println("no file or something error");
-				    }
-				JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-				//pane.add(picLabel);
-				canvas.add(picLabel);
+	canvas = new JPanel();
+	canvas.setPreferredSize(new Dimension(300,300));
+	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
+	canvas.setLocation(10,10);
+	BufferedImage myPicture = null;
+	try {
+	    myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
+	} catch(Exception e)
+	    { System.out.println("no file or something error");
+	    }
+	JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+	//pane.add(picLabel);
+	canvas.add(picLabel);
 
-				pane.add(canvas);
-				/* public void loadImages(File directory) throws IOException {
-					File[] imageFiles = directory.listFiles(fileNameFilter);
-					BufferedImage[] images = new BufferedImage[imageFiles.length];
-					model.removeAllElements();
-					for (int ii=0; ii<images.length; ii++) {
-					    model.addElement(ImageIO.read(imageFiles[ii]));
-					}
-					} */
+	pane.add(canvas);
+	/* public void loadImages(File directory) throws IOException {
+	   File[] imageFiles = directory.listFiles(fileNameFilter);
+	   BufferedImage[] images = new BufferedImage[imageFiles.length];
+	   model.removeAllElements();
+	   for (int ii=0; ii<images.length; ii++) {
+	   model.addElement(ImageIO.read(imageFiles[ii]));
+	   }
+	   } */
 
-				/*
-				pane.add(new JButton("button 0"));
-				JPanel buttongrid = new JPanel();
-				buttongrid.setLayout(new GridLayout(3,3));
-				buttongrid.add(new JButton("button 0"));
-				buttongrid.add(new JButton("button 1"));
-				buttongrid.add(new JButton("button 2"));
-				buttongrid.add(new JButton("button 3"));
-				buttongrid.add(new JButton("button 4"));
-				pane.add(buttongrid);
-				*/
+	/*
+	  pane.add(new JButton("button 0"));
+	  JPanel buttongrid = new JPanel();
+	  buttongrid.setLayout(new GridLayout(3,3));
+	  buttongrid.add(new JButton("button 0"));
+	  buttongrid.add(new JButton("button 1"));
+	  buttongrid.add(new JButton("button 2"));
+	  buttongrid.add(new JButton("button 3"));
+	  buttongrid.add(new JButton("button 4"));
+	  pane.add(buttongrid);
+	*/
 
-				//Image img = new ImageIcon(this.getClass().getResource("/Eiffel.jpg")).getImage();
+	//Image img = new ImageIcon(this.getClass().getResource("/Eiffel.jpg")).getImage();
 
-				/*	BufferedImage myPicture = null;
-				try {
-				    myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
-				} catch(Exception e)
-				    { System.out.println("no file or something error");
-				    }
-				JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-				pane.add(picLabel);
-				*/
-
+	/*	BufferedImage myPicture = null;
+		try {
+		myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
+		} catch(Exception e)
+		{ System.out.println("no file or something error");
 		}
+		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+		pane.add(picLabel);
+	*/
+
+    }
 
 
     public void imageAdder(int n, String name){
+	BufferedImage myPic = null;
 	for (int i = 0; i <= n; i++){
-	    BufferedImage myPic = null;
 	    try {
 		myPic = ImageIO.read(new File("Images/" + name + n + ".jpg"));
 	    } catch (Exception e){
