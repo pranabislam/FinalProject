215522f021ecc5e49cb432819df75e8fbdf1db3a
Pranab <Islam>
2015-01-18 02:14:14 -0500

    bunch of errors, but good start

---
+++ ImageSnapper.java
@@ -0,0 +1,14 @@
+import java.awt.Point
+
+public class ImageSnapper{
+    
+    Point[] = new Point[100];
+    Point[1] = Point(10, 10);
+    Point[2] = Point(40, 10);
+    Point[3] = Point(70, 10);
+    Point[4] = Point(100, 10);
+
+
+
+
+}
