fc9fc2d1da3051a93ec2188d3952567f07e58e7f
Remi Moon <remim00n7@gmail.com>
2015-01-19 20:01:36 -0500

    SAS

--- Gui1.java
+++ Gui1.java
@@ -23,8 +23,9 @@ import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import java.awt.GridLayout;
-
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
 
 
 public class Gui1 {
@@ -43,113 +44,40 @@ public class Gui1 {
 			ex.printStackTrace();
 		    }
 
-		    JFrame frame = new JFrame("Testing");  
-		 
+		    
+		    
 		    JLayeredPane pane = new JLayeredPane();
-		    /*
+		    JFrame frame = new JFrame("Testing");
+		    JPanel canvas1 = new JPanel();
+		    JPanel canvas2 = new JPanel();
+		    JPanel canvas3 = new JPanel();
+		    JPanel canvas4 = new JPanel();
+		    JPanel canvas5 = new JPanel();
+		    JPanel canvas6 = new JPanel();		   
+		    JPanel canvas7 = new JPanel();
+		    JPanel canvas8 = new JPanel();
+		    JPanel canvas9 = new JPanel();
+		    JPanel canvas10 = new JPanel();
+		    JPanel canvas11 = new JPanel();
+		    JPanel canvas12 = new JPanel();
+		    JPanel canvas13 = new JPanel();
+		    JPanel canvas14 = new JPanel();
+		    JPanel canvas15 = new JPanel();
+		    JPanel canvas16 = new JPanel();
 		    canvas1.setPreferredSize(new Dimension(300,300));
-		    canvas1.setBorder(BorderFactory.createLineBorder(Color.blue,10));
-		    canvas1.setVisible(true);
-		    canvas2.setPreferredSize(new Dimension(300,300));
-		    canvas2.setBorder(BorderFactory.createLineBorder(Color.black));
-		    canvas2.setVisible(true);
-	     		
+		    canvas1.setBorder(BorderFactory.createLineBorder(Color.blue,10));	       
 		    pane.add(canvas1, JLayeredPane.DEFAULT_LAYER);
-		    pane.add(canvas2, JLayeredPane.DEFAULT_LAYER);
-		    frame.add(pane);
-		    */
- frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    pane.setLayout(new GridLayout(4, 4));
-    pane.add(new JPanel("canvas 1"));
-    pane.add(new JPanel("canvas 2"));
-    pane.add(new JPanel("canvas 3"));
-     pane.add(new JPanel("canvas 4"));
-    pane.add(new JPanel("canvas 5"));
-    pane.add(new JPanel("canvas 6"));
-    pane.add(new JPanel("canvas 7"));
-    pane.add(new JPanel("canvas 8"));
-     pane.add(new JPanel("canvas 9"));
-   pane.add(new JPanel("canvas 10"));
-    pane.add(new JPanel("canvas 11"));
-    pane.add(new JPanel("canvas 12"));
-   pane.add(new JPanel("canvas 13"));
-    pane.add(new JPanel("canvas 14"));
-    pane.add(new JPanel("canvas 15"));
-    pane.add(new JPanel("canvas 16"));
-		    //   frame.add(new ImageGrab());
-    frame.add(pane);
+		   
+		    
+		    Rectangle rect1 = new Rectangle(20, 20, 40, 40);
+		    // pane.add(rect1, JLayeredPane.DEFAULT_LAYER);
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    frame.add(new ImageGrab());
 		    frame.pack();
 		    frame.setLocationRelativeTo(null);
-		    frame.setSize(800,800);
 		    frame.setVisible(true);
 		}
 	    });
 
     }
 }
-	//pane = getContentPane();
-	//pane.setLayout(new GridLayout(3,3));
-	/*	pane.setLayout(new FlowLayout());
-		b1 = new JButton("Click me");
-		pane.add(b1);
-		b2 = new JButton("exit");  
-		pane.add(b2);
-
-		label = new JLabel("The Label:");
-		pane.add(label);
-
-		text = new JTextArea();
-		text.setColumns(40);
-		text.setRows(5);
-		text.setBorder(BorderFactory.createLineBorder(Color.red,2));
-		pane.add(text);
-	*/
-
-	//	pane.add(canvas);
-
-	/*	BufferedImage myPicture = null;
-		try {
-		myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
-		} catch(Exception e)
-		{ System.out.println("no file or something error");
-		}
-		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-		//pane.add(picLabel);
-		canvas.add(picLabel);
-
-		pane.add(canvas);
-
-		/* public void loadImages(File directory) throws IOException {
-		File[] imageFiles = directory.listFiles(fileNameFilter);
-		BufferedImage[] images = new BufferedImage[imageFiles.length];
-		model.removeAllElements();
-		for (int ii=0; ii<images.length; ii++) {
-		model.addElement(ImageIO.read(imageFiles[ii]));
-		}
-		} */
-
-	/*
-	  pane.add(new JButton("button 0"));
-	  JPanel buttongrid = new JPanel();
-	  buttongrid.setLayout(new GridLayout(3,3));
-	  buttongrid.add(new JButton("button 0"));
-	  buttongrid.add(new JButton("button 1"));
-	  buttongrid.add(new JButton("button 2"));
-	  buttongrid.add(new JButton("button 3"));
-	  buttongrid.add(new JButton("button 4"));
-	  pane.add(buttongrid);
-	*/
-
-	//Image img = new ImageIcon(this.getClass().getResource("/Eiffel.jpg")).getImage();
-
-	/*	BufferedImage myPicture = null;
-		try {
-		myPicture = ImageIO.read(new File("Images/Eiffel.jpg"));
-		} catch(Exception e)
-		{ System.out.println("no file or something error");
-		}
-		JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-		pane.add(picLabel);
-	*/
-
-
