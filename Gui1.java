2f3dad48b73530731ca8ee70a8c82e56ddc0fea3
Pranab Islam <islampranab@gmail.com>
2015-01-20 20:11:19 -0500

    Finally added these freaking lines holy yessss
    freaking torture but got it

--- Gui1.java
+++ Gui1.java
@@ -55,7 +55,27 @@ public class Gui1 {
 		    
 		    //JLayeredPane pane = new JLayeredPane();
 		    JFrame frame = new JFrame("Testing");
-		    JPanel canvas = new JPanel();
+		      JPanel canvas = new JPanel(){
+			    public void paintComponent(Graphics g) {
+				super.paintComponent(g);
+
+				Graphics2D g2d = (Graphics2D) g;
+
+				float[] dash1 = { 2f, 0f, 2f };
+
+				g2d.drawLine(20, 40, 250, 40);
+
+				BasicStroke bs1 = new BasicStroke(1, 
+								  BasicStroke.CAP_BUTT, 
+								  BasicStroke.JOIN_ROUND, 
+								  1.0f, 
+								  dash1,
+								  2f);
+				g2d.setStroke(bs1);
+				g2d.drawLine(20, 80, 250, 80);
+
+			    }
+			};
 
 		    /* JPanel grid1 = new JPanel();		    
 		    JPanel grid2 = new JPanel();
