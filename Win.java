6b2cc55b13bfb9164265d526e9d2df9ab9cf2e89
Pranab Islam <islampranab@gmail.com>
2015-01-22 01:07:20 -0500

    Update Win.java

--- Win.java
+++ Win.java
@@ -6,7 +6,14 @@ import javax.swing.ImageIcon;
 
 public class Win{
 
-    
+    /* okay so messing with the image array in the other file is probably not smart because its very hard for us
+    to test anything, we can't print lines bcz of the gui in the main method.
+    so we do the testing separately and put a main method and tostring() in here i guess so we can test/print
+        i thought that we should do the files to img file array transfer multiple times and note the order of the array
+        if the order of the array holds constant with the order of the images 0-15, then we can individually name each image
+        0-15 (hopefully). from there you can begin to attempt the correct tile function because now java can easily refer to 
+        the image. the hard part is being able to name all the images from the array and do it knowing we got the right images 
+        in the right order. */
 
 }
     
